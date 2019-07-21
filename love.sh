5a69f7957ec3e7fd4b5b822ab35573c36f847937
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-07-21 22:29:30 +0530

    Update love.sh

--- love.sh
+++ love.sh
@@ -4,3 +4,5 @@ mkdir love
 curl https://raw.githubusercontent.com/rama2027/love/556ce64a0c263e868c3c622da96ee382210f095f/test.sh -o  my-script.sh
 chmod a+rx my-script.sh
 ./my-script.sh
+rm love
+rm test
