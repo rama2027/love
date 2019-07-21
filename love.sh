4ae608513e8a294c30260f1a8d586c8084ed9c0a
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-07-21 20:00:04 +0530

    Create love.sh

---
+++ love.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# This is a comment!
+curl https://raw.githubusercontent.com/rama2027/love/556ce64a0c263e868c3c622da96ee382210f095f/test.sh -o  my-script.sh
+chmod a+rx my-script.sh
+./my-script.sh
