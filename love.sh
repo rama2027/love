583c9da6ffe8a9015188faff630ca57016b8f32f
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-08-04 22:05:41 +0530

    Update love.sh

--- love.sh
+++ love.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 # This is a comment!
-mkdir love
-curl https://raw.githubusercontent.com/rama2027/love/556ce64a0c263e868c3c622da96ee382210f095f/test.sh -o  my-script.sh
-chmod a+rx my-script.sh
-./my-script.sh
-rm love.sh
-rm my-script.sh
+yum install python python-devel python-pip
+python-pip install boto
+python -c "import boto; print boto.Version"
