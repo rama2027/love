26f7ed431c7b1fb3945f42e1ea671810d2dc67a0
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-08-06 12:00:34 +0530

    Update ram.py

--- ram.py
+++ ram.py
@@ -2,8 +2,8 @@ import boto3
 import os
 client = boto3.client(
     'ec2',
-    aws_access_key_id="$AWS_ACCESS_KEY_ID",
-    aws_secret_access_key="$AWS_SECRET_ACCESS_KEY",
+    aws_access_key_id="AKIAVO4RZPOTCNCUGBF4",
+    aws_secret_access_key="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",
     region_name = 'eu-west-1'
 )
 response = client.describe_instances()
