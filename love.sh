3a12ab7d8db52f5752a06727fc0ff034f32f6042
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-08-06 09:05:08 +0530

    Update love.sh

--- love.sh
+++ love.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-git clone https://github.com/rama2027/love
+sudo cp /home/ec2-user/.aws /var/lib/jenkins/workspace/test/.aws
 virtualenv -p /usr/bin/python3 python3
 pip3 install boto3
-sudo cp /home/ec2-user/.aws /var/lib/jenkins/workspace/test/.aws
 python3 ram.py
