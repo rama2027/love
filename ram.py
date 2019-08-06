09a285e8b6478dc50c72a5a0b6557ae55664f3c4
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-08-06 11:27:53 +0530

    Update ram.py

--- ram.py
+++ ram.py
@@ -1,9 +1,9 @@
 import boto3
-ec2 = boto3.resource('ec2',region_name = 'eu-west-1')
-instance = ec2.create_instances(
-    ImageId = 'ami-0bbc25e23a7640b9b',
-    MinCount = 1,
-    MaxCount = 1,
-    InstanceType = 't2.micro',
-    KeyName = 'test')
-print (instance[0].id)
+client = boto3.client(
+    'ec2',
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_KEY,
+    aws_session_token=SESSION_TOKEN,
+)
+response = ec2.describe_instances()
+print(response)
