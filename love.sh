6580f58f86629042b349716fec79be1b1f587bca
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-08-06 10:24:58 +0530

    Update love.sh

--- love.sh
+++ love.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 mkdir ram3
 cd ram3
-git clone https://github.com/rama2027/love.git
-sudo cp -r /home/ec2-user/.aws /var/lib/jenkins/workspace/test/ram2/.aws
+sudo git clone https://github.com/rama2027/love.git
+sudo cp -r /home/ec2-user/.aws /var/lib/jenkins/workspace/test/ram3/.aws
 ls
 sudo pip3 install virtualenv
-virtualenv ram2
+virtualenv ram3
 source ram2/bin/activate
 pip3 install boto3
 python3 ram.py
