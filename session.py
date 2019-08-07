340d72e2479af23d9272c409b233009f097b3d89
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-08-08 04:39:01 +0530

    Update session.py

--- session.py
+++ session.py
@@ -1,9 +1,10 @@
 import boto3
 import os
 
+session = boto3.session.session(region_name = 'eu-west-1')
 sts = session.client('sts')
 
-assumed_role_object=sts.assume_role(
+assumed_role_object= sts.assume_role(
     RoleArn=os.environ['IAM_ROLE'],
     RoleSessionName="AssumeRoleSession1"
 )
@@ -12,8 +13,7 @@ resource = boto3.resource(
     'ec2',
     aws_access_key_id=credentials['AccessKeyId'],
     aws_secret_access_key=credentials['SecretAccessKey'],
-    aws_session_token=credentials['SessionToken'],
-    region_name = 'eu-west-1'
+    aws_session_token=credentials['SessionToken']
 )
 response = resource.describe_instances()
 print(response)
