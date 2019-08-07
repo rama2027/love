ce9b8ce163a349f7309b915fe671411667a3bd19
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-08-08 04:44:37 +0530

    Update session.py

--- session.py
+++ session.py
@@ -12,7 +12,7 @@ resource = boto3.resource(
     'ec2',
     aws_access_key_id=credentials['AccessKeyId'],
     aws_secret_access_key=credentials['SecretAccessKey'],
-    aws_session_token=credentials['SessionToken']
+    aws_session_token=credentials['SessionToken'],
     region_name = 'eu-west-1'
 )
 response = resource.describe_instances()
