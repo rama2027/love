e30ba4451e4ef6d8b22b5e4a0ec1d20f759a88d1
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-08-10 21:17:31 +0530

    Create s3.py

---
+++ s3.py
@@ -0,0 +1,21 @@
+import boto3
+import os
+
+boto_sts=boto3.client('sts')
+
+assumed_role_object = sts.assume_role(
+    RoleArn=os.environ['IAM_ROLE'],
+    RoleSessionName="love"
+)
+
+newsession_id = assumed_role_object["Credentials"]["AccessKeyId"]
+newsession_key = assumed_role_object["Credentials"]["SecretAccessKey"]
+newsession_token = assumed_role_object["Credentials"]["SessionToken"]
+
+s3 = boto3.client('s3',region_name='us-east-1',
+    aws_access_key_id=newsession_id,
+    aws_secret_access_key=newsession_key,
+    aws_session_token=newsession_token)
+    
+response = s3.create_bucket(Bucket='my-bucket')
+print(response)
