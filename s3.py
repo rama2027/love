bcb377dd4dc658e3efed6a23dd3cd8f2851e9ac9
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-08-11 23:39:52 +0530

    Update s3.py

--- s3.py
+++ s3.py
@@ -12,7 +12,7 @@ newsession_id = assumed_role_object["Credentials"]["AccessKeyId"]
 newsession_key = assumed_role_object["Credentials"]["SecretAccessKey"]
 newsession_token = assumed_role_object["Credentials"]["SessionToken"]
 
-s3 = boto3.client('s3',region_name='eu-west-1',LocationConstraint': 'eu-west-1',
+s3 = boto3.client('s3',region_name='eu-west-1',LocationConstraint':'eu-west-1',
     aws_access_key_id=newsession_id,
     aws_secret_access_key=newsession_key,
     aws_session_token=newsession_token)
