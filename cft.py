1ee58245a9cee2eafc037e9c078ff7a2b2bf845d
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-08-14 10:23:53 +0530

    Update cft.py

--- cft.py
+++ cft.py
@@ -18,6 +18,7 @@ s3 = boto3.client('s3',
     aws_secret_access_key=newsession_key,
     aws_session_token=newsession_token)
     
-s3.create_bucket(Bucket=os.environ['Bucket'],CreateBucketConfiguration={'LocationConstraint': 'os.environ['Location']'})
+lov = s3.create_bucket(Bucket=os.environ['Bucket'],CreateBucketConfiguration={'LocationConstraint': 'os.environ['Location']'})
+print(lov['Location'][0])
 
 
