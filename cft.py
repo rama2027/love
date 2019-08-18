bce4b360de0f49a30e51e7b7f1fc59d82bbaceba
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-08-18 18:06:39 +0530

    Update cft.py

--- cft.py
+++ cft.py
@@ -20,7 +20,7 @@ s3 = boto3.client('s3',
 Bucket=os.environ['Bucket']
 test = boto3.client('s3')
 bucket1 = test.get_bucket(Bucket)
-bucket_location = bucket1.get_location()
+bucket_location = bucket1.get_bucket_location()
 #lov = s3.create_bucket(Bucket=os.environ['Bucket'],CreateBucketConfiguration={'LocationConstraint': 'os.environ['Location']'})
 #url = s3.get_location(Bucketname=os.environ['Bucket'])['LocationConstraint': 'os.environ['Location']']
 print(bucket_location)
