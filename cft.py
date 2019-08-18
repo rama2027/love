5f1ab77970f8cf552c245c8f51c3334d09d25b7e
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-08-18 14:25:09 +0530

    Update cft.py

--- cft.py
+++ cft.py
@@ -17,9 +17,16 @@ s3 = boto3.client('s3',
     aws_access_key_id=newsession_id,
     aws_secret_access_key=newsession_key,
     aws_session_token=newsession_token)
-    
+
+Bucketname=os.environ['Bucket']
+file="test.json"
 lov = s3.create_bucket(Bucket=os.environ['Bucket'],CreateBucketConfiguration={'LocationConstraint': 'os.environ['Location']'})
-print(lov)
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(os.environ['Bucket'])
+bucket.upload_file("test.json", file)
+location = boto3.client('s3').get_bucket_location(Bucketname=os.environ['Bucket'])['LocationConstraint': 'os.environ['Location']']
+url = "https://s3-%s.amazonaws.com/%s/%s" % (location, bucket_name, key)
+print(url)
 
 
 
