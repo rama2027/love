f376f5b0f7c52af14e5e4f65c17d9e65a3de95fa
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-08-26 13:39:19 +0530

    Update cft.py

--- cft.py
+++ cft.py
@@ -20,6 +20,6 @@ s3 = boto3.client('s3',
 response = s3.create_bucket(Bucket=os.environ['Bucket'],CreateBucketConfiguration={'LocationConstraint': 'eu-west-1'})
 response = s3.list_buckets()
 Bucket=os.environ['Bucket']
-name = test.sh
+name = 'test.sh'
 print(response)
 response = s3_client.upload_file(name,Bucket,name)
