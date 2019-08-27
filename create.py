f3c2cc3fe0d2ab34836b8dd32295ed2de5c83da5
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-08-27 23:46:57 +0530

    Update create.py

--- create.py
+++ create.py
@@ -1,9 +1,7 @@
 import boto3
 import os
 
-ec2 = boto3.resource('ec2',region_name='eu-west-1',
-    aws_access_key_id=AKIAVO4RZPOTNPZMVD37,
-    aws_secret_access_key=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)
+ec2 = boto3.resource('ec2',region_name='eu-west-1',aws_access_key_id="AKIAVO4RZPOTNPZMVD37", aws_secret_access_key = "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")
 
 response = ec2.create_instances(
      ImageId='ami-0bbc25e23a7640b9b',
