781546f4e135cd8e2bb60dc7d2a5bfbfca1fde02
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-09-28 19:37:12 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -12,8 +12,8 @@ newsession_id = assumed_role_object["Credentials"]["AccessKeyId"]
 newsession_key = assumed_role_object["Credentials"]["SecretAccessKey"]
 newsession_token = assumed_role_object["Credentials"]["SessionToken"]
 
-export AWS_ACCESS_KEY_ID = newsession_key
-export AWS_SECRET_ACCESS_KEY = newsession_key
-export AWS_SESSION_TOKEN = newsession_token
+export AWS_ACCESS_KEY_ID=newsession_key
+export AWS_SECRET_ACCESS_KEY=newsession_key
+export AWS_SESSION_TOKEN=newsession_token
 
 print (os.environ['AWS_ACCESS_KEY_ID'])
