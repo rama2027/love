a6acf219c9348ed8235311a5cba8fd5cc9a21411
rama2027 <37704469+rama2027@users.noreply.github.com>
2019-09-28 17:29:57 +0530

    Create creds.py

---
+++ creds.py
@@ -0,0 +1,19 @@
+import boto3
+import os
+
+sts = boto3.client('sts')
+
+assumed_role_object = sts.assume_role(
+    RoleArn=os.environ['IAM_ROLE'],
+    RoleSessionName="love"
+)
+
+newsession_id = assumed_role_object["Credentials"]["AccessKeyId"]
+newsession_key = assumed_role_object["Credentials"]["SecretAccessKey"]
+newsession_token = assumed_role_object["Credentials"]["SessionToken"]
+
+export AWS_ACCESS_KEY_ID = newsession_key
+export AWS_SECRET_ACCESS_KEY = newsession_key
+export AWS_SESSION_TOKEN = newsession_token
+
+print (os.environ['AWS_ACCESS_KEY_ID'])
